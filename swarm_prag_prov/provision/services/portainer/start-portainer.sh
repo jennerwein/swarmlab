3da6c3459c523d0427f2cdf7bdd38b94ae3119d0
Martin Leischner <martin.leischner@h-brs.de>
2020-09-29 20:02:42 +0200

    Minor improvements

--- swarm_prag_prov/provision/services/portainer/start-portainer.sh
+++ swarm_prag_prov/provision/services/portainer/start-portainer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-GELB_AN="\033[30m\033[43m"
-GELB_AUS="\033[0m"
+YELLOW_ON="\033[30m\033[43m"
+YELLOW_OFF="\033[0m"
 
 cd /home/vagrant/provisioning/services/portainer
 
@@ -9,4 +9,4 @@ cd /home/vagrant/provisioning/services/portainer
 curl -L https://downloads.portainer.io/portainer-agent-stack.yml -o portainer-agent-stack.yml
 docker stack deploy --compose-file=portainer-agent-stack.yml portainer
 
-echo "$GELB_AN Portainer service started on http://172.16.0.101:9000$GELB_AUS"
+echo "$YELLOW_ON Portainer service started on http://172.16.0.101:9000$YELLOW_OFF"
--- swarm_prag_prov/provision/services/visualizer/start-visualizer.sh
+++ swarm_prag_prov/provision/services/visualizer/start-visualizer.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-GELB_AN="\033[30m\033[43m"
-GELB_AUS="\033[0m"
+YELLOW_ON="\033[30m\033[43m"
+YELLOW_OFF="\033[0m"
 
 cd /home/vagrant/provision/services/visualizer
 docker stack deploy --compose-file=visualizer.yml viz
 
-echo "$GELB_AN Visualizer service started on http://172.16.0.101:8050$GELB_AUS"
+echo "$YELLOW_ON Visualizer service started on http://172.16.0.101:8050$YELLOW_OFF"
 
