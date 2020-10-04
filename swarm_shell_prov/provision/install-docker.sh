d90896f74aa193b7e09d743c217f1d64c9d9f851
Zdih El Mehdi <5165595-zdih@users.noreply.gitlab.com>
2020-10-04 21:50:48 +0200

    Add the remarks discussed at the last appointment

--- swarm_shell_prov/provision/install-docker.sh
+++ swarm_shell_prov/provision/install-docker.sh
@@ -1,13 +1,19 @@
+# The scriptes can be found in the following link: 
+# https://docs.docker.com/engine/install/ubuntu/
 sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
-    software-properties-common
+    software-properties-common > /dev/null
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
+sudo apt-key fingerprint 0EBFCD88 > /dev/null
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
-   stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce
+   stable" > /dev/null
+sudo apt-get update > /dev/null
+sudo apt-get install -y docker-ce > /dev/null
+
+sudo groupadd docker
+
+sudo usermod -aG docker vagrant
