7e0182101f4dda301c3d885f1d2a6d8c91aabdcb
Zdih El Mehdi <5165595-zdih@users.noreply.gitlab.com>
2020-10-15 21:18:42 +0200

    some changes in aufgaben file

--- aufgaben/whoami/app.js
+++ aufgaben/whoami/app.js
@@ -1,5 +1,6 @@
 // Use the web application framework "express" 
 let app = require("express")();
+// Set the view engine to ejs
 app.set("view engine", "ejs");
 app.set("templates", __dirname + "/templates");
 // Import os and ip modules, required to get the hostname and the IP-Address of the current machine
@@ -9,7 +10,7 @@ let ip = require('ip');
 let hostName = os.hostname();
 // Get the IP-Address from "os" module
 let hostIp = ip.address();
-// The color will be based on the hostname defined  
+// The color will be based on the hostname defined 
 let color = hostName.substr(0, 5);
 // Send the IP, Hostname and Color to the template generator ejs
 app.get('/', (req, res) => {res.render("index", { hostName: hostName, hostIp: hostIp, color: color }); });
